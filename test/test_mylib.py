2c57f9dc21da000756e504ec10b2e335ae4ba0ee
Zoltan Csizmadia <CsizmadiaZ@valassis.com>
2022-04-05 14:15:43 -0500

    Add python based dockerfile

--- test/test_mylib.py
+++ test/test_mylib.py
@@ -1,13 +1,14 @@
-
-import sys 
+import sys
 sys.path.append("build")
 sys.path.append("build/Debug")
 from mylib import add, Add
 
-def test_add():
-     assert add(2, 5) == 7
 
-def test_Add():
+def test_add():
+    assert add(2, 5) == 7
+
+
+def test_add_class():
     adder = Add(0)
     adder.add(1)
     adder.add(2)
@@ -15,6 +16,7 @@ def test_Add():
     adder.add(4)
     assert adder.get_result() == 10
 
+
 if __name__ == '__main__':
     test_add()
-    test_Add()
+    test_add_class()
